32146377a0ead3448178cbc0113549fd870bdc80
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-26 14:12:26 +0200

    OCLint fails on compile errors

--- do_oclint.sh
+++ do_oclint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 # Clean main should work
-./oclint-0.10.3/bin/oclint -o oclint.log -max-priority-1 0 -max-priority-2 0 -max-priority-3 0 main.cpp -- -c > /dev/null
+./oclint-0.10.3/bin/oclint -o oclint.log main.cpp -- -c > /dev/null
+
+cat oclint.log
+
+# Will be 0 if success
+# Will be 1 if fail
+fail=`egrep "Compiler Errors" oclint.log | wc -l`
+
+if [ $fail -eq 1 ]; 
+then
+  echo "OCLint: Compiler error"
+  exit 1
+else
+  echo "OCLint: OK"
+fi
 
 # Will be 1 if success
 # Will be 0 if fail
